8b9c70eebb878d3c89d5ef9841e43dbed1759a1a
xiplus <huangxuanyuxiplus@gmail.com>
2016-10-31 15:49:16 +0800

    init

---
+++ main.js
@@ -0,0 +1,19 @@
+javascript:
+(function(){
+
+GridViewContent_RadioButtonList1_0_2_0.checked = true;
+GridViewContent_RadioButtonList1_1_0_1.checked = true;
+GridViewContent_RadioButtonList1_2_0_2.checked = true;
+GridViewContent_RadioButtonList1_3_1_3.checked = true;
+GridViewContent_RadioButtonList1_4_2_4.checked = true;
+GridViewContent_RadioButtonList1_5_2_5.checked = true;
+GridViewContent_CheckBoxList1_6_0_6.checked = true;
+GridViewContent_RadioButtonList1_8_1_8.checked = true;
+GridViewContent_RadioButtonList1_9_0_9.checked = true;
+GridViewContent_RadioButtonList1_10_0_10.checked = true;
+GridViewContent_RadioButtonList1_11_1_11.checked = true;
+GridViewContent_RadioButtonList1_12_1_12.checked = true;
+GridViewContent_RadioButtonList1_13_2_13.checked = true;
+ButtonSend.click();
+
+})();
